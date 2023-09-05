1fd4fe99140c5b0f84175de485f33fb8ff8bb41c
Мария Кислякова <viridis.ber@yandex.ru>
2023-09-05 12:31:04 +0300

    ДО БАЗОВОГО ВЬЮ ДЕТАИЛА

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
 
 
 class BirthdayMixin:
@@ -31,23 +30,9 @@ class BirthdayUpdateView(BirthdayMixin, UpdateView):
     pass
 
 
-# Добавим опциональный параметр pk.
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None, files=request.FILES or None, instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
 def delete_birthday(request, pk):
